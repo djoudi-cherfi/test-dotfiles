69edbb2f278f97afe336508064bd37aa4e66c5e4
Djoudi <github.h1m5z@aleeas.com>
2023-12-29 04:30:00 +0100

    add src, os folder  and utils file

--- setup.sh
+++ setup.sh
@@ -1,7 +1,62 @@
 #!/usr/bin/env bash
 
+declare -r DOTFILES_UTILS_URL="https://raw.githubusercontent.com/$GITHUB_REPOSITORY/main/src/os/utils.sh"
+
+download() {
+
+    local url="$1"
+    local output="$2"
+
+    if command -v "curl" &> /dev/null; then
+
+        curl \
+            --location \
+            --silent \
+            --show-error \
+            --output "$output" \
+            "$url" \
+                &> /dev/null
+
+        return $?
+
+    elif command -v "wget" &> /dev/null; then
+
+        wget \
+            --quiet \
+            --output-document="$output" \
+            "$url" \
+                &> /dev/null
+
+        return $?
+    fi
+
+    return 1
+
+}
+
+download_utils() {
+
+    local tmpFile=""
+
+    tmpFile="$(mktemp /tmp/XXXXX)"
+
+    download "$DOTFILES_UTILS_URL" "$tmpFile" \
+        && . "$tmpFile" \
+        && rm -rf "$tmpFile" \
+        && return 0
+
+   return 1
+
+}
+
 main() {
-    echo "Hello world!"
+    cd "$(dirname "${BASH_SOURCE[0]}")" || exit 1
+
+    if [ -x "utils.sh" ]; then
+        . "utils.sh" || exit 1
+    else
+        download_utils || exit 1
+    fi
 }
 
 main
