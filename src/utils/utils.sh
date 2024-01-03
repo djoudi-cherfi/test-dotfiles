e4da6981d5197d11d5ed8fb39c63b4adc129f619
Djoudi <github.h1m5z@aleeas.com>
2024-01-03 01:51:55 +0100

    update v1

--- src/utils/utils.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cd "$(dirname "${BASH_SOURCE[0]}")" \
-    && . "./print.sh" \
-    && . "./execute.sh" \
-    && . "./process.sh" \
-    && . "./spinner.sh"
-
-echo "Test utils !"
